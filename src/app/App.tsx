907b92947846080d96a62f313db9ae7fc4f47b00
Mark Golubtsov <glmarkgl4@gmail.com>
2022-11-16 23:37:56 +0300

    Change modal trigger.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,29 +1,24 @@
-import React, {useEffect, useState} from "react";
-import {Modal} from "antd";
+import React, {useState} from "react";
+import {Button, Modal} from "antd";
 import ReportBuilder from "app/view/ReportBuilder";
 
 import './App.less';
 
-const TRIGGER_KEY = 'r';
-
 export default function App() {
     const [visible, setVisible] = useState(false);
 
-    useEffect(() => {
-        const handleKeyDown = (event: KeyboardEvent) => {
-            if (event.key.toLowerCase() === TRIGGER_KEY) {
-                setVisible(true);
-            }
-        }
-
-        window.addEventListener('keydown', handleKeyDown);
-        return () => window.removeEventListener('keydown', handleKeyDown);
-    }, []);
-
     return (
-        <Modal destroyOnClose footer={null} open={visible} title='Git lab issues report' onCancel={handleClose}>
-            <ReportBuilder/>
-        </Modal>
+        <>
+            {
+                !visible &&
+                <Button className='report-trigger' onClick={() => setVisible(true)}>
+                    Open report form
+                </Button>
+            }
+            <Modal destroyOnClose footer={null} open={visible} title='Git lab issues report' onCancel={handleClose}>
+                <ReportBuilder/>
+            </Modal>
+        </>
     )
 
     function handleClose() {
